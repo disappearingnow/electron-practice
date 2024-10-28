447709c4c8eb34fb93af85bdf5850e1bb582a35a
disappearingnow <disappearing.now@gmail.com>
2024-10-28 16:16:31 -0400

    test app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useState, FormEvent } from "react";
+import { TextInput } from "./components/_index";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  const [textInput, setTextInput] = useState("");
+  const [displayText, setDisplayText] = useState("");
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setDisplayText(textInput);
+    setTextInput("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="testInput">Test input:</label>
+        <TextInput
+          name="testInput"
+          id="testInput"
+          value={textInput}
+          setValue={setTextInput}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h3>What you typed:</h3>
+        <p>{displayText}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
